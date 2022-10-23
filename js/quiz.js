8103da8c28b1f9f8d199c1523dddfa97f229e6da
cherry-aisha <113786104+cherry-aisha@users.noreply.github.com>
2022-10-23 13:52:16 +0100

    Variables added @quiz.js

---
+++ js/quiz.js
@@ -0,0 +1,13 @@
+// Quiz Components
+var welcome = document.getElementById("welcome")
+var timer = document.getElementById("timer")
+var startQuiz = document.getElementById("startQuiz")
+var quiz = document.getElementById("quiz")
+var options = document.getElementById("options")
+var message = document.getElementById("message")
+var result = document.getElementById("result")
+var saveScore = document.getElementById("saveScore")
+var viewScores = document.getElementById("viewScores")
+var summary = document.getElementById("summary")
+var playAgain = document.getElementById("playAgain")
+
